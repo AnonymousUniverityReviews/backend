60596e5c0d2cfff3024da30d0c17504c59cea312
pichus <dimatyzuk@gmail.com>
2025-09-04 21:02:45 +0300

    add swagger

---
+++ src/AnonymousStudentReviews.Api/Configurations/MiddlewareConfig.cs
@@ -0,0 +1,18 @@
+namespace AnonymousStudentReviews.Api.Configurations;
+
+public static class MiddlewareConfig
+{
+    public static IApplicationBuilder UseAppMiddleware(this WebApplication app)
+    {
+        app.UseHttpsRedirection();
+        
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseOpenApi();
+            app.UseSwaggerUi();
+            app.MapGet("/", () => Results.Redirect("/swagger"));
+        }
+        
+        return app;
+    }
+}
